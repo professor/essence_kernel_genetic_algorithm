6e0a5d11e00e067da64b8f3f38abc99d05ccdc24
Todd Sedano <todd.sedano@sv.cmu.edu>
2014-11-15 10:58:20 -0800

    Add comparison for score hash

--- lib/essence_kernel_genetic_algorithm/empirical_data.rb
+++ lib/essence_kernel_genetic_algorithm/empirical_data.rb
@@ -44,6 +44,23 @@ class EmpiricalData
     total_score
     score_hash
   end
+
+  def self.print_comparison_between(original_score_hash, candidate_score_hash)
+    original_score_hash.each do |team_id, score|
+      next if team_id == :total
+      candidate_score = candidate_score_hash[team_id]
+      puts "#{team_id}: #{score} vs #{candidate_score} #{candidate_score > score ? 'BETTER' : ''}"
+    end
+
+    team_id = :total
+    score = original_score_hash[team_id]
+    candidate_score = candidate_score_hash[team_id]
+    puts "#{team_id}: #{score} vs #{candidate_score} #{candidate_score > score ? 'BETTER' : ''}"
+  end
+
+  def self.is_candidate_score_better(original_score_hash, candidate_score_hash)
+    return original_score_hash[:total] < candidate_score_hash[:total]
+  end
 end
 #
 # kernel_json_string = File.read(File.expand_path('../../../spec/fixtures/CMU_1.1.json', __FILE__))
--- spec/empirical_data_spec.rb
+++ spec/empirical_data_spec.rb
@@ -22,4 +22,26 @@ describe EmpiricalData do
     expect(score_hash[143]).to eq 1239
     expect(score_hash[148]).to eq 151
   end
+
+  context '#is_candidate_score_better' do
+    let(:original_score_hash) { {part1: 10, part2: 90, total: 100} }
+
+    it 'original is just better' do
+      candidate_score_hash = {part1: 5, part2: 5, total: 10}
+      result = EmpiricalData.is_candidate_score_better(original_score_hash, candidate_score_hash)
+      expect(result).to eq false
+    end
+
+    it 'they are tied' do
+      result = EmpiricalData.is_candidate_score_better(original_score_hash, original_score_hash)
+      expect(result).to eq false
+    end
+
+    it 'candidate is just better' do
+      candidate_score_hash = {part1: 15, part2: 95, total: 110}
+      result = EmpiricalData.is_candidate_score_better(original_score_hash, candidate_score_hash)
+      expect(result).to eq true
+    end
+  end
+
 end
