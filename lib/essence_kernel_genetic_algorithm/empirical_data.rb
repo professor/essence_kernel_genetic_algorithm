4b67cc10152fa68496cb4f9ab326aecefe399a88
Todd Sedano <todd.sedano@sv.cmu.edu>
2014-11-15 10:32:55 -0800

    Added simple test to EmpricialData, refactored into class

--- lib/essence_kernel_genetic_algorithm/empirical_data.rb
+++ lib/essence_kernel_genetic_algorithm/empirical_data.rb
@@ -1,35 +1,56 @@
 require_relative './individual.rb'
 require 'JSON'
+class EmpiricalData
 
-
-kernel_json_string = File.read(File.expand_path('../../../spec/fixtures/CMU_1.1.json', __FILE__))
-individual = Individual.from_json_string(kernel_json_string)
-
-team_id = 26
-team_json_string = File.read(File.expand_path("../../../lib/generated_json/team_#{team_id}_deltas.json", __FILE__))
-team_json = JSON.parse(team_json_string)
-
-def consider(individual, team_json, checklist_id, meeting_index)
-  return 0 if checklist_id < 0
-  score = 0
-  current_meeting = meeting_index
-  while current_meeting < team_json.size
-    team_json[meeting_index].keys.each do |checklist|
-      continue if checklist.to_i < 0
-      score += 1 if individual.before(checklist_id, checklist.to_i)  #Todo fix json generator to not use strings
+  def self.load_team_data
+    team_data = {}
+    team_ids = [21, 26, 108, 121, 124, 143, 148]
+    team_ids.each do |team_id|
+      team_json_string = File.read(File.expand_path("../../../lib/generated_json/team_#{team_id}_deltas.json", __FILE__))
+      team_data[team_id] = JSON.parse(team_json_string)
     end
-    current_meeting += 1
+    team_data
   end
-  score
-end
 
-score = 0
-team_json.each_with_index do |meeting, meeting_index|
-  meeting.keys.each do |checklist_id|
-    score += consider(individual, team_json, checklist_id.to_i, meeting_index)
+  def self.consider(individual, team_json, checklist_id, meeting_index)
+    return 0 if checklist_id < 0
+    score = 0
+    current_meeting = meeting_index
+    while current_meeting < team_json.size
+      team_json[meeting_index].keys.each do |checklist|
+        next if checklist.to_i < 0
+        score += 1 if individual.before(checklist_id, checklist.to_i)  #Todo fix json generator to not use strings
+      end
+      current_meeting += 1
+    end
+    score
+  end
+
+  def self.evaluate(individual, team_data)
+    score_hash = {}
+    total_score = 0
+    team_data.each do |id, team_json|
+      score_for_team = 0
+      team_json.each_with_index do |meeting, meeting_index|
+        meeting.keys.each do |checklist_id|
+          score_for_team += consider(individual, team_json, checklist_id.to_i, meeting_index)
+        end
+      end
+      puts "#{id} => #{score_for_team}"
+      score_hash[id] = score_for_team
+      total_score += score_for_team
+    end
+    score_hash[:total] = total_score
+    total_score
+    score_hash
   end
 end
-
-puts score
-
+#
+# kernel_json_string = File.read(File.expand_path('../../../spec/fixtures/CMU_1.1.json', __FILE__))
+# individual = Individual.from_json_string(kernel_json_string)
+#
+# team_data = EmpiricalData.load_team_data
+# score_hash = EmpiricalData.evaluate(individual, team_data)
+# puts score_hash[:total]
+#
 
