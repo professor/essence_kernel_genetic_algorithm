c47c40fee0d521b9b1fb9662e8732e34119fb6d7
Todd Sedano <todd.sedano@sv.cmu.edu>
2014-11-15 15:19:41 -0800

    Added systematic evaluation of simple move

---
+++ lib/essence_kernel_genetic_algorithm/simple_algorithm.rb
@@ -0,0 +1,41 @@
+require_relative './individual.rb'
+require_relative './operators.rb'
+require_relative './empirical_data.rb'
+
+class SimpleAlgorithm
+
+  def systematically_move_checklists_one_state(direction)
+    kernel_json_string = File.read(File.expand_path('../../../spec/fixtures/CMU_1.1.json', __FILE__))
+    original = Individual.from_json_string(kernel_json_string)
+
+    team_data = EmpiricalData.load_team_data
+    original_score_hash = EmpiricalData.evaluate(original, team_data)
+
+    #iterate through all checklists
+    original.number_of_alphas.times do |alpha_index|
+      from = {alpha: alpha_index}
+
+      number_of_states = original.number_of_states(from)
+      number_of_states.times do |state_index|
+        from = {alpha: alpha_index, state: state_index}
+
+        number_of_checklists = original.number_of_checklists(from)
+        number_of_checklists.times do |checklist_index|
+          from = {alpha: alpha_index, state: state_index, checklist: checklist_index}
+
+          individual = Marshal.load(Marshal.dump(original))
+
+          Operators.move_checklist_one_state_later(individual, from) if (direction == :later)
+          Operators.move_checklist_one_state_earlier(individual, from) if (direction == :earlier)
+
+          candidate_score_hash = EmpiricalData.evaluate(original, team_data)
+
+          if EmpiricalData.is_candidate_score_better(original_score_hash, candidate_score_hash)
+            individual.pretty_print
+            EmpiricalData.print_comparison_between(original_score_hash, candidate_score_hash)
+          end
+        end
+      end
+    end
+  end
+end
---
+++ spec/simple_algorithm_spec.rb
@@ -0,0 +1,15 @@
+require_relative '../lib/essence_kernel_genetic_algorithm/simple_algorithm'
+
+describe SimpleAlgorithm do
+
+  let(:kernel_json_string) { File.read(File.expand_path('../fixtures/CMU_1.1.json', __FILE__)) }
+  let(:individual) { Individual.from_json_string(kernel_json_string) }
+
+  xit 'moves every checklist item one state later' do
+    subject.systematically_move_checklists_one_state(:later)
+  end
+
+  xit 'moves every checklist item one state earlier' do
+    subject.systematically_move_checklists_one_state(:earlier)
+  end
+end
