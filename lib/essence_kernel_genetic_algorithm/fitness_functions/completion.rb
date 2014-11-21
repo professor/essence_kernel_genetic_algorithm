c575914ae7c6a2ce8e3b871af4efe0680db555e8
Todd Sedano <todd.sedano@sv.cmu.edu>
2014-11-21 08:08:13 -0800

    Completion fitness function now works with multiple states

--- lib/essence_kernel_genetic_algorithm/fitness_functions/completion.rb
+++ lib/essence_kernel_genetic_algorithm/fitness_functions/completion.rb
@@ -4,30 +4,35 @@ class Completion
     score_hash = {}
     total_score = 0
 
-    # individual.number_of_alphas.times do |alpha_index|
-    #   from = {alpha: alpha_index}
-    #
-    #   number_of_states = individual.number_of_states(from)
-    #   number_of_states.times do |state_index|
-    #     from = {alpha: alpha_index, state: state_index}
-    #
-    #     number_of_checklists = individual.number_of_checklists(from)
-    #     number_of_checklists.times do |checklist_index|
-    #       from = {alpha: alpha_index, state: state_index, checklist: checklist_index}
-    #
-    #       checklist_id = starting_individual.lookup(from)['id']
-    #
-    #
-    #     end
-    #   end
-    # end
-
-    from = {alpha: 0, state: 0, checklist: 0}
-    checklist_id = individual.lookup(from)['id']
-
-    Hash.new
     team_data = team_data_collection.teams.values.first
-    total_score += 1 if team_data.contains?(checklist_id)
+
+    #iterate
+    individual.number_of_alphas.times do |alpha_index|
+      from = {alpha: alpha_index}
+
+      earlier_checklist_ids = []
+      current_states_checklist_ids = []
+
+      number_of_states = individual.number_of_states(from)
+      number_of_states.times do |state_index|
+        from = {alpha: alpha_index, state: state_index}
+
+        earlier_checklist_ids.concat current_states_checklist_ids
+        current_states_checklist_ids = []
+
+        number_of_checklists = individual.number_of_checklists(from)
+        number_of_checklists.times do |checklist_index|
+          from = {alpha: alpha_index, state: state_index, checklist: checklist_index}
+
+          current_checklist_id = individual.lookup(from)['id']
+          # binding.pry
+          total_score += 1 if team_data.contains?(current_checklist_id) and
+            team_data.before?(earlier_checklist_ids, current_checklist_id)
+
+          current_states_checklist_ids << current_checklist_id
+        end
+      end
+    end
 
     score_hash[:total] = total_score
     score_hash
--- spec/completion_spec.rb
+++ spec/completion_spec.rb
@@ -5,22 +5,106 @@ require_relative '../lib/essence_kernel_genetic_algorithm/team_data_collection'
 describe Completion do
 
   context '#evaluate' do
-    let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_one_state_one_checklist_each.json', __FILE__)) }
-    let(:individual) { Individual.from_json_string(json_string) }
+    context 'trivial case of one state with one checklist' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_one_state_one_checklist.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
 
-    it 'works with trivial case' do
-      team_data = TeamDataCollection.create_example([{'1' => true }])
-      score_hash = Completion.evaluate(individual, team_data)
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }])
+        score_hash = Completion.evaluate(individual, team_data)
 
-      expect(score_hash[:total]).to eq 1
+        expect(score_hash[:total]).to eq 1
+      end
+
+      it 'does not match data' do
+        team_data = TeamDataCollection.create_example([{'99' => true }])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 0
+      end
     end
 
-    it 'works with trivial case' do
-      team_data = TeamDataCollection.create_example([{'99' => true }])
-      score_hash = Completion.evaluate(individual, team_data)
+    context 'two states with one checklist each' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_two_states_one_checklist.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
 
-      expect(score_hash[:total]).to eq 0
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true}])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2
+      end
+
+      it 'incorrect order of states' do
+        team_data = TeamDataCollection.create_example([{'2' => true }, {'1' => true}])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 1
+      end
     end
 
+    context 'two states in different alphas with one checklist each' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/two_alphas_one_state_one_checklist.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
+
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true}])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2
+      end
+    end
+
+    context 'one alpha with one state with two checklists' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_one_state_two_checklists.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
+
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true}])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2
+      end
+    end
+
+    context 'one alpha with two states with two checklists' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_two_states_two_checklists.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
+
+      it 'matches sequential data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true}, {'11' => true}, {'12' => true} ])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 4
+      end
+
+      it 'matches data when data happens during same week' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true, '11' => true}, {'12' => true} ])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 3  #or 4 depending upon definitions
+      end
+
+      it 'matches data when happens all at the same time' do
+        team_data = TeamDataCollection.create_example([{'1' => true, '2' => true, '11' => true, '12' => true} ])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2 #or 4 if we count them all happening at the same time.
+      end
+
+      it 'incomplete states only scores items in first state' do
+        team_data = TeamDataCollection.create_example([{'1' => true}, {'11' => true}, {'12' => true} ])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 1
+      end
+
+      it 'swapped states only scores items in first state' do
+        team_data = TeamDataCollection.create_example([{'11' => true}, {'12' => true}, {'1' => true}, {'2' => true} ])
+        score_hash = Completion.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2
+      end
+    end
   end
 end
