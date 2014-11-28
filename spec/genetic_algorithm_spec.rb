12b19964f9e99e256031ef84d719f2f578fa4fb1
Todd Sedano <professor@gmail.com>
2014-11-28 12:19:40 -0800

    Test suite generation of 3 operators

--- spec/genetic_algorithm_spec.rb
+++ spec/genetic_algorithm_spec.rb
@@ -7,8 +7,8 @@ describe GeneticAlgorithm do
   it 'works' do
     Random.srand(1234)
 
-    subject.run(Completion, {runs: 1, generations: 10, number_of_alphas: 42})
-    # subject.run(PartialOrdering, {runs: 1, generations: 1000, number_of_alphas: 42})
+    subject.run(Completion, {runs: 1, generations: 11, number_of_alphas: 42})
+    subject.run(PartialOrdering, {runs: 1, generations: 11, number_of_alphas: 42})
 
   end
 
