8f690bea738778dd4328ea1158f56a0e9ba2b2a9
Todd Sedano <professor@gmail.com>
2014-11-29 09:18:54 -0800

    Adding test cases to partial ordering and fixing algorithm correctness

--- lib/essence_kernel_genetic_algorithm/fitness_functions/partial_ordering.rb
+++ lib/essence_kernel_genetic_algorithm/fitness_functions/partial_ordering.rb
@@ -1,34 +1,50 @@
 class PartialOrdering
 
-  def self.consider(individual, team, checklist_id, meeting_index)
-    return 0 if checklist_id < 0
+  def self.score_before_or_equals(individual, checklist_id, later_checklist_ids)
     score = 0
-    current_meeting = meeting_index
-    while current_meeting < team.meetings.size
-      team.meetings[meeting_index].keys.each do |checklist|
-        next if checklist.to_i < 0
-        score += 1 if individual.before(checklist_id, checklist.to_i)  #Todo fix json generator to not use strings
-      end
-      current_meeting += 1
+    later_checklist_ids.each_key do |id|
+      score += 1 if id != checklist_id && individual.before_or_equals(checklist_id.to_i, id.to_i)
     end
     score
   end
 
+  def self.score_equals_or_after(individual, earlier_checklist_ids, checklist_id)
+    score = 0
+    earlier_checklist_ids.each_key do |id|
+      score += 1 if id != checklist_id && individual.equals_or_after(checklist_id.to_i, id.to_i)
+    end
+    score
+  end
+
+  def self.score_one_team_data(individual, team_data)
+    score = 0
+
+    earlier_checklist_ids = {}
+    later_checklist_ids = team_data.checklists.clone
+
+    team_data.meetings.each do |meeting|
+      meeting.keys.each do |checklist_id_string|
+        checklist_id = checklist_id_string.to_i
+        score += score_before_or_equals(individual, checklist_id, later_checklist_ids)
+        score += score_equals_or_after(individual, earlier_checklist_ids, checklist_id)
+
+        earlier_checklist_ids[checklist_id] = true
+        later_checklist_ids.delete(checklist_id)
+      end
+    end
+    score
+  end
+
+  public
   def self.evaluate(individual, team_data_collection)
     score_hash = {}
     total_score = 0
     team_data_collection.teams.each do |id, team|
-      score_for_team = 0
-      team.meetings.each_with_index do |meeting, meeting_index|
-        meeting.keys.each do |checklist_id|
-          score_for_team += consider(individual, team, checklist_id.to_i, meeting_index)
-        end
-      end
+      score_for_team = score_one_team_data(individual, team)
       score_hash[id] = score_for_team
       total_score += score_for_team
     end
     score_hash[:total] = total_score
-    total_score
     score_hash
   end
 
--- spec/partial_ordering_spec.rb
+++ spec/partial_ordering_spec.rb
@@ -9,16 +9,134 @@ describe PartialOrdering do
   it 'calculates a total score' do
     team_data = EmpiricalData.load_team_data
     score_hash = PartialOrdering.evaluate(individual, team_data)
-    expect(score_hash[:total]).to eq 11583
+    expect(score_hash[:total]).to eq 13372
   end
 
   it 'calculates a score for each teams dataset' do
     team_data = EmpiricalData.load_team_data
     score_hash = PartialOrdering.evaluate(individual, team_data)
-    expect(score_hash[21]).to eq 1238
-    expect(score_hash[26]).to eq 2272
-    expect(score_hash[108]).to eq 2620
-    expect(score_hash[121]).to eq 2152
-    expect(score_hash[124]).to eq 3301
+    expect(score_hash[21]).to eq 2430
+    expect(score_hash[26]).to eq 2794
+    expect(score_hash[108]).to eq 2676
+    expect(score_hash[121]).to eq 2724
+    expect(score_hash[124]).to eq 2748
+  end
+
+  context '#evaluate' do
+    context 'trivial case of one state with one checklist' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_one_state_one_checklist.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
+
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 0 #there is no relationship with anything else
+      end
+
+      it 'does not match data' do
+        team_data = TeamDataCollection.create_example([{'99' => true }])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 0
+      end
+    end
+
+    context 'two states with one checklist each' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_two_states_one_checklist.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
+
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true}])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2
+      end
+
+      it 'incorrect order of states' do
+        team_data = TeamDataCollection.create_example([{'2' => true }, {'1' => true}])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 0
+      end
+    end
+
+    context 'two states in different alphas with one checklist each' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/two_alphas_one_state_one_checklist.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
+
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true}])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 0
+      end
+    end
+
+    context 'one alpha with one state with two checklists' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_one_state_two_checklists.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
+
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true}])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2
+      end
+
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'1' => true, '2' => true}])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2
+      end
+
+      it 'matches data' do
+        team_data = TeamDataCollection.create_example([{'2' => true, '1' => true}])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq 2
+      end
+    end
+
+    context 'one alpha with two states with two checklists' do
+      let(:json_string) { File.read(File.expand_path('../fixtures/one_alpha_two_states_two_checklists.json', __FILE__)) }
+      let(:individual) { Individual.from_json_string(json_string) }
+
+      it 'matches sequential data' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true}, {'11' => true}, {'12' => true} ])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq (4 * 3)
+      end
+
+      it 'matches data when data happens during same week' do
+        team_data = TeamDataCollection.create_example([{'1' => true }, {'2' => true, '11' => true}, {'12' => true} ])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq (4 * 3)
+      end
+
+      it 'matches data when happens all at the same time' do
+        team_data = TeamDataCollection.create_example([{'1' => true, '2' => true, '11' => true, '12' => true} ])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq  (4 * 3)
+      end
+
+      it 'incomplete states only scores items in first state' do
+        team_data = TeamDataCollection.create_example([{'1' => true}, {'11' => true}, {'12' => true} ])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq (3 * 2)
+      end
+
+      it 'swapped states only scores items in first state' do
+        team_data = TeamDataCollection.create_example([{'11' => true}, {'12' => true}, {'1' => true}, {'2' => true} ])
+        score_hash = PartialOrdering.evaluate(individual, team_data)
+
+        expect(score_hash[:total]).to eq (2 + 2)
+      end
+    end
   end
 end
