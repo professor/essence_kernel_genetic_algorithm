7f0697848f5e6bd4f022bb86633d04bae5541090
Todd Sedano <professor@gmail.com>
2014-12-09 20:44:08 -0800

    Updated examples on how to run it with normal parameters

--- spec/genetic_algorithm_spec.rb
+++ spec/genetic_algorithm_spec.rb
@@ -4,12 +4,19 @@ require_relative '../lib/essence_kernel_genetic_algorithm/fitness_functions/comp
 
 describe GeneticAlgorithm do
 
-  it 'works' do
+  it 'for automated testing, we set the parameters low' do
     Random.srand(1234)
 
     subject.run(Completion, {runs: 1, generations: 11, number_of_alphas: 42})
     subject.run(PartialOrdering, {runs: 1, generations: 11, number_of_alphas: 42})
 
+  end
+
+  it 'for a real run use these' do
+    Random.srand(1234)
+
+    # subject.run(Completion, {runs: 40, generations: 120, number_of_alphas: 8})
+    # subject.run(PartialOrdering, {runs: 40, generations: 120, number_of_alphas: 4})
   end
 
 end
